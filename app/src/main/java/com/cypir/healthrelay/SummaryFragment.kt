8efac84b8b1cdf96e27cd2257a6bc2c0a5476bec
Alex Nguyen <amn77a@gmail.com>
2018-07-26 02:54:02 +0000

    added units to interval text. Fixes #4.

--- app/src/main/java/com/cypir/healthrelay/SummaryFragment.kt
+++ app/src/main/java/com/cypir/healthrelay/SummaryFragment.kt
@@ -11,12 +11,6 @@ import android.view.ViewGroup
 import com.cypir.healthrelay.viewmodel.MainViewModel
 import kotlinx.android.synthetic.main.fragment_summary.*
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 /**
  * A simple [Fragment] subclass.
  * Activities that contain this fragment must implement the
